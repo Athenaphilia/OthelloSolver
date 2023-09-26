516f220ebf2eedef3f2e3869e1042a5b144c44fd
Corey Aras Kaulenas <corey.kaulenas@gmail.com>
2023-09-25 22:16:15 -0400

    Created Node struct for solver

--- solver.c
+++ solver.c
@@ -1,5 +1,14 @@
 #include "solver.h"
+#include "position.h"
+#include <math.h>
 
-double evaluation(Bitboard board, int player) {
-    return 0;
-}
+// USC! exploration constant
+#define UCB1_C 1.0
+
+typedef struct Node {
+    Game game;
+    Node *parent;
+    Node *children[34];
+    uint64_t visits;
+    double value;
+} Node;
